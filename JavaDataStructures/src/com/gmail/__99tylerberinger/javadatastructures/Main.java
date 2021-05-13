ac98be7d5d92e69786f99bf88773ff378b34f2b8
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-13 19:46:59 -0400

    Added .gitignore, created Main.java

---
+++ JavaDataStructures/src/com/gmail/__99tylerberinger/javadatastructures/Main.java
@@ -0,0 +1,11 @@
+package com.gmail.__99tylerberinger.javadatastructures;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("java-data-structures\n");
+
+    }
+
+}
