d403f2f72c7abf0b8c177385ac922239f530ce86
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-20 21:00:38 -0400

    Added methods peek(), indexOf(), clear(), contains(), size()

--- JavaDataStructures/src/com/gmail/__99tylerberinger/javadatastructures/things/Stack.java
+++ JavaDataStructures/src/com/gmail/__99tylerberinger/javadatastructures/things/Stack.java
@@ -15,6 +15,30 @@ public class Stack {
 
     private Node firstNode = null;
 
+    public final int peek() {
+
+        return (firstNode != null ? firstNode.data : -1);
+
+    }
+
+    public final int indexOf(int data) {
+
+        Node dataNode = firstNode;
+        int i = 0;
+        while (dataNode != null) {
+            if (dataNode.data == data) {
+
+                return i;
+
+            }
+            dataNode = dataNode.nextNode;
+            i += 1;
+        }
+
+        return -1;
+
+    }
+
     public final void push(int data) {
 
         final Node newNode = new Node(data);
@@ -48,6 +72,44 @@ public class Stack {
         }
 
         return -1;
+
+    }
+
+    public final void clear() {
+
+        firstNode = null;
+
+        printStack();
+
+    }
+
+    public final boolean contains(int data) {
+
+        Node dataNode = firstNode;
+        while (dataNode != null) {
+            if (dataNode.data == data) {
+
+                return true;
+
+            }
+            dataNode = dataNode.nextNode;
+        }
+
+        return false;
+
+    }
+
+    public final int size() {
+
+        int size = 0;
+
+        Node node = firstNode;
+        while (node != null) {
+            size += 1;
+            node = node.nextNode;
+        }
+
+        return size;
 
     }
 
