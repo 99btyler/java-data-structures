542b040ec400ed1bece264991f9f30bef9a856c4
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-24 17:20:27 -0400

    Added Node

--- JavaDataStructures/src/com/gmail/__99tylerberinger/javadatastructures/things/BinarySearchTree.java
+++ JavaDataStructures/src/com/gmail/__99tylerberinger/javadatastructures/things/BinarySearchTree.java
@@ -2,4 +2,18 @@ package com.gmail.__99tylerberinger.javadatastructures.things;
 
 public class BinarySearchTree {
 
+    private class Node {
+
+        public int data;
+        public Node leftChildNode = null;
+        public Node rightChildNode = null;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+    }
+
+    private Node firstNode = null;
+
 }
